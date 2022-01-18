2ece2e8ed3d0ad2699da3ef6cfcfc231682f6018
Ihor Korenets <ihor_korenets@epam.com>
2022-01-18 17:51:53 +0200

    tuned started styles, fonts, colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import type {AppProps} from 'next/app';
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps) {
+    return <>
+        <Head>
+            <title>MyTop App</title>
+            <link rel="icon" href="/favicon.ico"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                  rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+    </>;
 }
 
 export default MyApp;
