f7d667a042b1048f1492d8d563e46dbe38b9be11
Ihor Korenets <ihor_korenets@epam.com>
2022-01-25 17:26:33 +0200

    made styles for Layout and Footer

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -3,17 +3,16 @@ import { Sidebar } from './Sidebar/Sidebar';
 import { Header } from './Header/Header';
 import { Footer } from './Footer/Footer';
 import { FunctionComponent } from 'react';
+import styles from './Layout.module.css';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
-		<>
-			<Header />
-			<div>
-				<Sidebar />
-				<div>{children}</div>
-			</div>
-			<Footer />
-		</>
+		<div className={styles.wrapper}>
+			<Header className={styles.header} />
+			<Sidebar className={styles.sidebar} />
+			<div className={styles.body}>{children}</div>
+			<Footer className={styles.footer} />
+		</div>
 	);
 };
 
